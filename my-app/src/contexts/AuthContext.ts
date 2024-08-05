ba53fe6f3dc1aaf503ee4e6d3ed6f2fd4d5c8699
João Victor Carvalho <joaocarvoli@hotmail.com>
2024-08-04 23:09:03 -0300

    refact: Recreating Login page and adding auth management

---
+++ my-app/src/contexts/AuthContext.ts
@@ -0,0 +1,61 @@
+'use client'
+
+import User from '@/model/User';
+import { api } from '@/services/axios';
+import React, { createContext, useEffect, useState } from 'react';
+import { setCookie, parseCookies } from 'nookies';
+import { useRouter } from 'next/navigation'
+// import { recoverUserInfo } from '@/api/user';
+
+
+type AuthContextType = {
+  isAuthenticated: boolean;
+  user: User | null;
+  signIn: (data: SignInData) => Promise<void>;
+}
+
+type UserData = {
+  token: string,
+  user: User
+}
+
+type SignInData = {
+  email: string;
+  password: string;
+}
+
+export const AuthContext = createContext({} as AuthContextType);
+
+export function AuthProvider({children}: Readonly<{children: React.ReactNode}>){
+  const router = useRouter()
+  const [user, setUser] = useState<User | null>(null)
+  const isAuthenticated = !!user;
+
+  useEffect(() => {
+    const { 'quixalert.auth.token': token } = parseCookies()
+    // if(token) recoverUserInfo(token).then(user => setUser(user))
+  })
+
+  async function signIn({ email, password }: SignInData){
+    const response = await api.post<UserData>('login', {
+      email: email,
+      password: password
+    })
+    
+    setCookie(undefined, 'quixalert.auth.token', response.data.token, {
+      maxAge: 60 * 60 * 8 // 8 hours
+    })
+
+    api.defaults.headers['Authorization'] = `Bearer ${response.data.token}`
+
+    setUser(response.data.user)
+
+    router.push('/home')
+  }
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, user, signIn }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
