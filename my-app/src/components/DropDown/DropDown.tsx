2499e8f00a2d1b233f2e90f5e948fbfb18b1495f
jonatas <jonatas.brito.ferreira@gmail.com>
2024-08-05 15:13:38 -0300

    feat: add new navbar

---
+++ my-app/src/components/DropDown/DropDown.tsx
@@ -0,0 +1,34 @@
+import React, {useState} from "react"
+
+import DropDownItem from "../DropDownItem/DropDownItem";
+
+import userImg from "@/../../public/assets/images/user.png";
+
+import {DownOutlined, FormOutlined, LogoutOutlined, UserOutlined} from "@ant-design/icons";
+
+import "./style.css"
+
+const DropDown = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div>
+      <div className="user-menu" >
+        <img className="user-image" src={userImg.src}/>
+        <div className="user-details">
+          <div className="user-name" >Phablo Gabriel</div>
+          <div className="user-role" >Administrador</div>
+        </div>
+
+        <DownOutlined className="arrow-item" onClick={() => setOpen(!open)} />
+      </div>
+      <ul className={`dropdown-container ${open? ' active' : ' inactive'}`} >
+        <DropDownItem icon={<UserOutlined />} text="Meu perfil"/>
+        <DropDownItem icon={<FormOutlined />} text="Atendidos"/>
+        <DropDownItem icon={<LogoutOutlined />} text="Sair"/>
+      </ul>
+    </div>
+  )
+}
+
+export default DropDown
