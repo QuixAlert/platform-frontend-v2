f7bbaa91e751c137e4ee9f2929382b0f65d85ad2
João Victor Carvalho <joaocarvoli@hotmail.com>
2024-08-15 10:28:16 -0300

    refact: Using new error structures

--- my-app/src/api/adoptions.ts
+++ my-app/src/api/adoptions.ts
@@ -1,35 +1,38 @@
-import {parseCookies} from "nookies";
 import Adoption from "@/model/Adoption";
-import {HttpStatusCode} from "axios";
-import {logout} from "@/lib/utils";
+import { HttpStatusCode } from "axios";
+import { ForbiddenError } from "@/errors/forbidden";
+import { EmptyResponseError } from "@/errors/empty-response";
 
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL_API
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL_API;
 
 export const fetchAdoptions = async (token: string) => {
-    try {
-        const response = await fetch(`${baseUrl}/adoption`, {
-            headers: {Authorization: `Bearer ${token}`},
-            method: 'GET'
-        })
+  const response = await fetch(`${baseUrl}/adoption`, {
+    headers: { Authorization: `Bearer ${token}` },
+    method: "GET",
+  })
 
-        if(response.status == HttpStatusCode.Forbidden) logout()
+  if (response.status == HttpStatusCode.Forbidden) throw new ForbiddenError("User Login Failed")
 
-        return await response.json() as Adoption[]
-    } catch (e) {
-        return [] as Adoption[]
-    }
-}
+  const adoptions = (await response.json()) as Adoption[];
+
+  if (adoptions.length == 0) throw new EmptyResponseError("No adoptions found")
+  
+  throw new EmptyResponseError("No adoptions found")
+
+  return adoptions
+};
+
 export const fetchAdoption = async (token: string, id: string) => {
-    try {
-        const response = await fetch(`${baseUrl}/adoption/${id}`, {
-            headers: {Authorization: `Bearer ${token}`},
-            method: 'GET'
-        })
+  try {
+    const response = await fetch(`${baseUrl}/adoption/${id}`, {
+      headers: { Authorization: `Bearer ${token}` },
+      method: "GET",
+    });
 
-        if(response.status == HttpStatusCode.Forbidden) logout()
+    // if(response.status == HttpStatusCode.Forbidden) logout()
 
-        return await response.json() as Adoption
-    } catch (e) {
-        return {} as Adoption
-    }
-}
+    return (await response.json()) as Adoption;
+  } catch (e) {
+    return {} as Adoption;
+  }
+};
