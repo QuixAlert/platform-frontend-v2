97cb84da76c46678e09419202cff00eee1c9bbab
João Victor Carvalho <joaocarvoli@hotmail.com>
2024-08-17 16:49:26 -0300

    feat: New error

--- my-app/src/errors/forbidden.ts
+++ my-app/src/errors/forbidden.ts
@@ -1,9 +1,11 @@
+import {ErrorName} from "@/errors/error-names";
+
 export class ForbiddenError extends Error {
 
   constructor(message: string) {
 
     super(message);
 
-    this.name = ErrorName.FORIBIDDEN
+    this.name = ErrorName.FORBIDDEN
   }
 }
