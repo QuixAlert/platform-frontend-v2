eefbd1f8d8eadea24fb58aa0c60a28dd6e727f21
jonatas <jonatas.brito.ferreira@gmail.com>
2024-09-03 14:41:37 -0300

    feat: change sidebar and navbar layout root system

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -1,10 +1,21 @@
-import type { Metadata } from "next";
-import { Nunito } from "next/font/google";
-import "./globals.css";
-import {AuthProvider} from "@/contexts/AuthContext";
 import React from "react";
+
+import type { Metadata } from "next";
+
+import {AuthProvider} from "@/contexts/AuthContext";
+
 import NavBar from "@/components/NavBar/NavBar";
 import Sidebar from "@/components/SideBar/SideBar";
+
+import Layout from "antd/lib/layout";
+import Sider from "antd/lib/layout/Sider";
+import { Header } from "antd/lib/layout/layout";
+import Content from "antd/lib/layout";
+
+import { Nunito } from "next/font/google";
+
+import "./globals.css";
+import SideBar from "@/components/SideBar/SideBar";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -24,9 +35,29 @@ export default function RootLayout({
       <link rel="icon" href="QuixAlert! 5.png" />
       </head>
       <body className="${font.className} bg-default">
-        <AuthProvider>
-          {children}
-        </AuthProvider>
+        <Layout className={"min-h-screen h-auto"}>
+          <Header className="p-0">
+            <NavBar/>
+          </Header>
+          <Layout>
+            <Sider width={"5rem"}>
+              <SideBar />
+            </Sider>
+            <Layout>
+              <Content
+                style={{
+                  margin: 0,
+                  minHeight: 280,
+                  height: "100%"
+                }}
+              >
+                <AuthProvider>
+                  {children}
+                </AuthProvider>
+              </Content>
+            </Layout>
+          </Layout>
+        </Layout>
       </body>
     </html>
   );
