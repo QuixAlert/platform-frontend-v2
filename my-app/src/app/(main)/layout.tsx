c0a53d351bef3934ce92fee792cf6559ed31fb01
jonatas <jonatas.brito.ferreira@gmail.com>
2024-09-11 11:08:27 -0300

    style: fix style in adoption page list

--- my-app/src/app/(main)/layout.tsx
+++ my-app/src/app/(main)/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
       <head>
       <link rel="icon" href="QuixAlert! 5.png" />
       </head>
-      <body className={`${font.className} bg-default`}>
+      <body className={`${font.className} bg-pgb`}>
         <AntdRegistry>
           <Layout className={"min-h-screen h-auto"}>
           <Header className="p-0">
