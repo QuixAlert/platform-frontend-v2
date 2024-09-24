a1c6e43bf9e59108eb214462fd7fa57520540d8c
João Victor Carvalho <joaocarvoli@hotmail.com>
2024-09-23 22:58:47 -0300

    feat: creating unauthorized page

--- my-app/src/app/(main)/users/page.tsx
+++ my-app/src/app/(main)/users/page.tsx
@@ -1,14 +1,19 @@
 "use client"
 
-import {Suspense, useState} from "react";
-import { FloatButton, notification, Tooltip } from "antd";
-import { PlusCircleOutlined } from "@ant-design/icons";
+import {Suspense, useState, useEffect} from "react";
+import {FloatButton, notification, Tooltip} from "antd";
+import {PlusCircleOutlined} from "@ant-design/icons";
 import UserModal from "@/components/User/UserModal";
-import { Loading } from "@/components/ui/Loading/Loading";
-import { UsersListFetcher } from "@/components/User/Fetcher/UserListFetcher";
+import {Loading} from "@/components/ui/Loading/Loading";
+import {UsersListFetcher} from "@/components/User/Fetcher/UserListFetcher";
+import {userInfoStore} from "@/store/user";
+import {Role} from "@/model/Role";
+import {useRouter} from "next/navigation"; // Import useRouter
 
 export default function UsersPage() {
     const [api, contextHolder] = notification.useNotification();
+    const { user } = userInfoStore();
+    const router = useRouter(); // Initialize useRouter
 
     const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -19,6 +24,19 @@ export default function UsersPage() {
     const handleModalClose = () => {
         setIsModalOpen(false);
     };
+
+    console.log(user?.role)
+    console.log(Role.ADMIN)
+
+    useEffect(() => {
+        if (user?.role !== Role.ADMIN) {
+            router.push('/unauthorized');
+        }
+    }, [user, router]);
+
+    if (!user) {
+        return null;
+    }
 
     return (
         <>
@@ -35,7 +53,7 @@ export default function UsersPage() {
                 <Tooltip placement="left" title="Adicionar novo usuário">
                     <FloatButton onClick={showModal} icon={<PlusCircleOutlined />} />
                 </Tooltip>
-                
+
                 <UserModal open={isModalOpen} onClose={handleModalClose} notificationApi={api}/>
             </div>
         </>
