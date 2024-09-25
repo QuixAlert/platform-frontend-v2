a8abb10bf9c435e33b77510ac5dee6886bddcbef
João Victor Carvalho <joaocarvoli@hotmail.com>
2024-09-25 01:19:45 -0300

    refact: updating on development page documents

---
+++ my-app/src/components/InDevelopment/InDevelopment.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import { ArrowLeftOutlined } from "@ant-design/icons";
+import Link from "next/link";
+import ColorButton from "@/components/Button/ColorButton";
+
+type InDevelopmentComponentProps = {
+    pageName: string;
+}
+
+function InDevelopmentComponent({ pageName }: InDevelopmentComponentProps) {
+    return (
+        <div className="flex flex-col justify-center items-center min-h-full min-w-full bg-pgb text-center p-4">
+            <Image
+                priority={true}
+                alt="Quixalert logo"
+                src="/exceptions/register-failed.svg"
+                width={400}
+                height={400}
+                className="mb-8"
+            />
+
+            <h1 className="text-6xl font-bold font-mono text-white mb-4">
+                Oops!
+            </h1>
+
+            <p className="text-lg text-gray-400 mb-8">
+                A página de {pageName} ainda está em desenvolvimento.
+            </p>
+
+            <Link href="/home">
+                <ColorButton bgColor="#269996" type="primary" icon={<ArrowLeftOutlined />}>
+                    Voltar para Home
+                </ColorButton>
+            </Link>
+        </div>
+    );
+}
+
+export default InDevelopmentComponent;
